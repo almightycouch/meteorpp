e394d4579e752ba55bf56912c6ba7e7b5a77736b
Mario Flach <info@almightycouch.org>
2015-09-09 22:53:14 +0200

    Use pkg-config to load EJDB from submodule

--- cmake/Modules/FindEjdb.cmake
+++
@@ -1,9 +0,0 @@
-set(Ejdb_FOUND true)
-
-if(Ejdb_FOUND)
-    set(Ejdb_LIBRARIES ejdb)
-    set(Ejdb_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/ejdb/dist/include")
-    link_directories("${PROJECT_SOURCE_DIR}/ejdb/dist/lib")
-endif()
-
-mark_as_advanced(Ejdb_INCLUDE_DIRS Ejdb_LIBRARIES)
