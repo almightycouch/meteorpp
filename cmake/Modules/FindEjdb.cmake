8cb15a72f4cef1f4e0f8d63b17c1b7baf1012929
Mario Flach <info@almightycouch.org>
2015-09-09 05:14:14 +0200

    Modify CMake to install EJDB to ejb/dist/ dir

--- cmake/Modules/FindEjdb.cmake
+++ cmake/Modules/FindEjdb.cmake
@@ -1,7 +1,9 @@
 set(Ejdb_FOUND true)
 
 if(Ejdb_FOUND)
-  set(Ejdb_LIBRARIES ejdb)
+    set(Ejdb_LIBRARIES ejdb)
+    set(Ejdb_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/ejdb/dist/include")
+    link_directories("${PROJECT_SOURCE_DIR}/ejdb/dist/lib")
 endif()
 
 mark_as_advanced(Ejdb_INCLUDE_DIRS Ejdb_LIBRARIES)
