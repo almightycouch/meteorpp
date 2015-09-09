570ba8a1df61693b1f5011a79f17014e655cc7cb
Mario Flach <info@almightycouch.org>
2015-09-09 19:19:49 +0200

    Add more test cases to minimongo test suite

--- tests/minimongo.cpp
+++ tests/minimongo.cpp
@@ -31,10 +31,103 @@ BOOST_FIXTURE_TEST_CASE(insert_with_invalid_oid, fixture)
     BOOST_CHECK_THROW(coll->insert({{ "_id", oid }, { "foo", "bar" }}), meteorpp::ejdb_exception);
 }
 
-BOOST_FIXTURE_TEST_CASE(insert_and_find_first, fixture)
+BOOST_FIXTURE_TEST_CASE(insert_find_one, fixture)
 {
-    nlohmann::json document = {{ "foo", "bar" }};
-    auto const id = coll->insert(document);
-    document["_id"] = id;
-    BOOST_CHECK_EQUAL(coll->find_one(), document);
+    nlohmann::json::object_t doc = {{ "foo", "bar" }};
+    doc.insert(std::make_pair("_id", coll->insert(doc)));
+    BOOST_CHECK_EQUAL(coll->find_one(), doc);
+}
+
+BOOST_FIXTURE_TEST_CASE(insert_find_multi, fixture)
+{
+    nlohmann::json::object_t doc1 = {{ "foo", "bar" }};
+    doc1.insert(std::make_pair("_id", coll->insert(doc1)));
+
+    nlohmann::json::object_t doc2 = {{ "foo", "baz" }};
+    doc2.insert(std::make_pair("_id", coll->insert(doc2)));
+
+    auto const excepted = { doc1, doc2 };
+    BOOST_CHECK_EQUAL(coll->find(), excepted);
+}
+
+BOOST_FIXTURE_TEST_CASE(insert_update_one, fixture)
+{
+    nlohmann::json::object_t doc = {{ "foo", "bar" }};
+    doc.insert(std::make_pair("_id", coll->insert(doc)));
+
+    auto const it = coll->update(doc, {{ "$set", {{ "foo", "baz" }}}});
+    BOOST_CHECK_EQUAL(it, 1);
+
+    doc["foo"] = "baz";
+    BOOST_CHECK_EQUAL(coll->find_one(), doc);
+}
+
+BOOST_FIXTURE_TEST_CASE(insert_update_multi, fixture)
+{
+    nlohmann::json::object_t doc1 = {{ "foo", "bar" }};
+    doc1.insert(std::make_pair("_id", coll->insert(doc1)));
+
+    nlohmann::json::object_t doc2 = {{ "foo", "baz" }};
+    doc2.insert(std::make_pair("_id", coll->insert(doc2)));
+
+    auto const it = coll->update({}, {{ "$set", {{ "bar", "foo" }}}});
+    BOOST_CHECK_EQUAL(it, 2);
+
+    doc1["bar"] = "foo";
+    doc2["bar"] = "foo";
+
+    auto const excepted = { doc1, doc2 };
+    BOOST_CHECK_EQUAL(coll->find(), excepted);
+}
+
+BOOST_FIXTURE_TEST_CASE(insert_multi_update_one, fixture)
+{
+    nlohmann::json::object_t doc1 = {{ "foo", "bar" }};
+    doc1.insert(std::make_pair("_id", coll->insert(doc1)));
+
+    nlohmann::json::object_t doc2 = {{ "foo", "baz" }};
+    doc2.insert(std::make_pair("_id", coll->insert(doc2)));
+
+    auto const it = coll->update(doc1, {{ "$set", {{ "bar", "foo" }}}});
+    BOOST_CHECK_EQUAL(it, 1);
+
+    doc1["bar"] = "foo";
+
+    auto const result_vect = coll->find();
+    std::set<nlohmann::json::object_t> const result_set(result_vect.begin(), result_vect.end());
+    std::set<nlohmann::json::object_t> const excepted = { doc1, doc2 };
+    BOOST_CHECK_EQUAL(result_set, excepted);
+}
+
+BOOST_FIXTURE_TEST_CASE(insert_remove_one, fixture)
+{
+    auto const id = coll->insert({{ "foo", "bar" }});
+    auto const it = coll->remove({{ "_id", id }});
+
+    BOOST_CHECK_EQUAL(it, 1);
+    BOOST_CHECK(coll->find_one().empty());
+}
+
+BOOST_FIXTURE_TEST_CASE(insert_remove_multi, fixture)
+{
+    coll->insert({{ "foo", "bar" }});
+    coll->insert({{ "foo", "baz" }});
+    auto const it = coll->remove({});
+
+    BOOST_CHECK_EQUAL(it, 2);
+    BOOST_CHECK(coll->find_one().empty());
+}
+
+BOOST_FIXTURE_TEST_CASE(insert_multi_remove_one, fixture)
+{
+    nlohmann::json::object_t doc1 = {{ "foo", "bar" }};
+    doc1.insert(std::make_pair("_id", coll->insert(doc1)));
+
+    nlohmann::json::object_t doc2 = {{ "foo", "baz" }};
+    doc2.insert(std::make_pair("_id", coll->insert(doc2)));
+
+    auto const it = coll->remove(doc1);
+
+    BOOST_CHECK_EQUAL(it, 1);
+    BOOST_CHECK_EQUAL(coll->find_one(), doc2);
 }
